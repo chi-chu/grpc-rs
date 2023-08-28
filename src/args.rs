7963b1396fcf945659d53d397cf71844f1c715a2
彳亍 <544625106@qq.com>
2023-08-28 18:59:30 +0800

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,13 @@
 use std::env;
 use chrono::Local;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::env;
+use std::string::String;
+use std::sync::RwLock;
 
 const PORT: u16 = 8080;
+static CMD: Lazy<RwLock<HashMap<String, String>>> = Lazy::new(|| RwLock::new(HashMap::new()));
 
 pub fn get_port() -> u16 {
 	match env::args().nth(1) {
@@ -19,4 +25,31 @@ pub fn get_port() -> u16 {
 			PORT
 		},
 	}
+}
+
+pub fn init_cmd_line() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let mut map = CMD.write().unwrap();
+    let mut i = 0;
+    'a: loop {
+        if i + 3 >= args.len() {
+            break 'a;
+        }
+        map.insert(args[i].replace("-", "").to_owned(), args[i + 1].to_owned());
+        i += 2;
+    }
+}
+
+pub fn is_grpc() -> bool {
+    let g = CMD.read().unwrap();
+    return match g.get("protocol") {
+        None => false,
+        Some(protocol) => {
+            if protocol.as_str() == "grpc" {
+                return true;
+            }
+            false
+        }
+    };
 }
